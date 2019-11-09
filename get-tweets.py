354a0cf6617fbfce15fdf68e9a3bccdf8b1dc462
Jhonatan Araujo <jhonatan.araujo@dafiti.com.br>
2019-11-09 19:49:34 -0300

    [dev/app] Working with list and for

--- get-tweets.py
+++ get-tweets.py
@@ -9,7 +9,7 @@ consumer_key = 'MRrZ7A56epXQp8ZEOeBpAwOHz'
 consumer_secret = '4vh8l5ylVFriebna5AUKcMcNlwhoblC3ux8iF6WyIAG0EGWqg8'
 access_token = '61522884-KoyXmgBrmG1Fn0UTO3nzjaxLxWs2ZBNBajzxVYiaa'
 access_token_secret = 'sB2t1MB7kOaG2pAKLR9WXtnq8xu5r1XXH09d4rwgTOGw9'
-word = sys.argv[1]
+hashtags_list = ['#openbanking','#apifirst','#devops','#cloudfirst','#microservices','#apigateway','#oauth','#swagger','#raml','#openapis']
 
 # Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -17,9 +17,11 @@ auth.set_access_token(access_token, access_token_secret)
 twitter = tweepy.API(auth)
 
 # Searching twitters
-results = twitter.search(q="%s" % (word))
-for tweet in results:
-    user = tweet.user.screen_name
-    followers = tweet.user.followers_count
-    message = tweet.text
-    print("User: %s - Followers: %s - Tweet: %s" % (user, followers, message))
+for hashtags in hashtags_list:
+    print("Hashtag: %s \n" % (hashtags))
+    results = twitter.search(q="%s" % (hashtags))
+    for tweet in results:
+        user = tweet.user.screen_name
+        followers = tweet.user.followers_count
+        message = tweet.text
+    print("User: %s - Followers: %s - Tweet: %s \n" % (user, followers, message))
