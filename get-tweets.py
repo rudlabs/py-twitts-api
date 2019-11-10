672942ffeddade9d193bc89e18950c9f1efc3ea6
Jhonatan Araujo <jhonatan.araujo@dafiti.com.br>
2019-11-10 02:27:22 -0300

    [dev/app] Creating a function

--- get-tweets.py
+++ get-tweets.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python2.7
+#!/usr/local/bin/python3
 
-# Importing modules
-import sys
 import tweepy
 
 # Variables
@@ -9,19 +7,16 @@ consumer_key = 'MRrZ7A56epXQp8ZEOeBpAwOHz'
 consumer_secret = '4vh8l5ylVFriebna5AUKcMcNlwhoblC3ux8iF6WyIAG0EGWqg8'
 access_token = '61522884-KoyXmgBrmG1Fn0UTO3nzjaxLxWs2ZBNBajzxVYiaa'
 access_token_secret = 'sB2t1MB7kOaG2pAKLR9WXtnq8xu5r1XXH09d4rwgTOGw9'
-hashtags_list = ['#openbanking','#apifirst','#devops','#cloudfirst','#microservices','#apigateway','#oauth','#swagger','#raml','#openapis']
 
 # Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 twitter = tweepy.API(auth)
 
-# Searching twitters
-for hashtags in hashtags_list:
-    print("Hashtag: %s \n" % (hashtags))
-    results = twitter.search(q="%s" % (hashtags))
-    for tweet in results:
+def search_hashtag():
+    hashtag = input('Informe uma Hashtag: ')
+    for tweet in tweepy.Cursor(twitter.search, q=(hashtag)).items(100):
         user = tweet.user.screen_name
         followers = tweet.user.followers_count
-        message = tweet.text
-    print("User: %s - Followers: %s - Tweet: %s \n" % (user, followers, message))
+        print("User: %s - Followers: %s \n" % (user, followers))
+search_hashtag()
