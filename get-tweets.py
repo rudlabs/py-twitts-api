2cfcac012232e2c74808652665df735f52c1af11
Jhonatan Araujo <jhonatan.araujo@dafiti.com.br>
2019-11-10 14:27:53 -0300

    [dev/app] Setting class

--- get-tweets.py
+++ get-tweets.py
@@ -1,6 +1,8 @@
 #!/usr/local/bin/python3
 
 import tweepy
+from flask import Flask
+from flask_restful import Resource, Api
 
 # Variables
 consumer_key = 'MRrZ7A56epXQp8ZEOeBpAwOHz'
@@ -8,15 +10,22 @@ consumer_secret = '4vh8l5ylVFriebna5AUKcMcNlwhoblC3ux8iF6WyIAG0EGWqg8'
 access_token = '61522884-KoyXmgBrmG1Fn0UTO3nzjaxLxWs2ZBNBajzxVYiaa'
 access_token_secret = 'sB2t1MB7kOaG2pAKLR9WXtnq8xu5r1XXH09d4rwgTOGw9'
 
+app = Flask(__name__)
+api = Api(app)
+
 # Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 twitter = tweepy.API(auth)
 
-def search_hashtag():
-    hashtag = input('Informe uma Hashtag: ')
-    for tweet in tweepy.Cursor(twitter.search, q=(hashtag)).items(100):
-        user = tweet.user.screen_name
-        followers = tweet.user.followers_count
-        print("User: %s - Followers: %s \n" % (user, followers))
-search_hashtag()
+class GetTweet(Resource):
+    def get(self):
+        hashtag = '#LulaLivre'
+        for tweet in tweepy.Cursor(twitter.search, q=(hashtag)).items(5):
+            user = tweet.user
+            return(str(user))
+
+api.add_resource(GetTweet, '/gettweet')
+
+if __name__ == '__main__':
+    app.run(debug=True)
